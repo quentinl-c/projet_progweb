c57831f63d9eee976e88ddd1373cec2ed1369da6
Quentin Laporte-Chabasse <quentin.laporte-chabasse@telecomnancy.net>
2015-04-13 23:57:38 +0200

    Add sign up template

--- stp/dispatcher.py
+++ stp/dispatcher.py
@@ -25,7 +25,7 @@ import handler
 
 class MainHandler(handler.Handler):
 	def get(self):
-		self.render("login.html")
+		self.render("signup.html")
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler)
