1684af1708fb2a18cda7ec5c7605700b316d4f74
Quentin Laporte-Chabasse <quentin.laporte-chabasse@telecomnancy.net>
2015-04-13 23:31:14 +0200

    Improve templates inheritance and add login template

--- stp/dispatcher.py
+++ stp/dispatcher.py
@@ -25,7 +25,7 @@ import handler
 
 class MainHandler(handler.Handler):
 	def get(self):
-		self.render("front.html")
+		self.render("login.html")
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler)
