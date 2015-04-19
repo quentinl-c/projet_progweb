b3e4b9e64aabe6af472404f7c07194bf550f3b70
Remi Even <even.remi@gmail.com>
2015-04-19 22:25:38 +0200

    Fixed some problems in addTask controller

--- stp/controller/addTask.py
+++ stp/controller/addTask.py
@@ -3,66 +3,58 @@
 from handler import Handler
 from TaskOffer import TaskOffer
 from user import User
+from cookie import read_secure, is_valid_and_secure
 
 import datetime
 
-
 class AddTask(Handler):
-
 	def get(self):
-		creatorLoginID = str(self.request.cookies.get("userId")).split('|')[0]
-
-		if creatorLoginID == None:
-			redirect('/')
+		if is_valid_and_secure(self, "userId") :
+			self.render("addtask.html")
 		else :
-			self.render("addTask.html")
+			self.redirect('/login')
 
 	def post(self):
-
-		self.title = self.request.get("title")
-		self.comment = self.request.get("comment")
-		self.sameAd = self.request.get("sameAd")
-		LoginID = str(self.request.cookies.get("userId")).split('|')[0]
-		if LoginID!=None:
-			creatorLoginID=int(LoginID)
-		if self.sameAd:
-			self.address = User.get_by_id(creatorLoginID).address
+		creatorID = read_secure(self, "userId")
+		if not is_valid_and_secure(self, "userId") :
+			redirect('/login')
+		creatorID = read_secure(self, "userId")
+		title = self.request.get("title")
+		comment = self.request.get("comment")
+		sameAd = self.request.get("sameAd")
+		if sameAd:
+			address = User.get_by_id(int(creatorID)).address
 		else:
-			self.address = self.request.get("address")
-			if self.address == "":
-				self.address=None
-		self.date=self.request.get("date")
-		
+			address = self.request.get("address")
+			if address == "":
+				address = None
+		date = self.request.get("date")
 
-		params = dict(title = self.title,
-					  comment = self.comment,
-					  date = self.date)
+		params = dict(title = title,
+					  comment = comment,
+					  date = date)
 		have_error = False
 
-		if self.title == "":
+		if not title :
 			params["error_title"] = "La tache n'a pas de titre."
 			have_error = True
 
-		if self.comment == "":
-			params["error_comment"] = "La tache n'a pas de descripton."
+		if not comment :
+			params["error_comment"] = "La tache n'a pas de description."
 			have_error = True
 
-		if self.date == "":
+		if not date :
 			params["error_date"] = "La tache n'a pas de date."
 			have_error = True
 		else:
-			self.date = datetime.datetime.strptime(self.request.get("date"), "%Y-%m-%d")
-			#date = self.date.date().isoformat()
+			date = datetime.datetime.strptime(date, "%Y-%m-%d")
+			#TODO : test the date is correct (ie not in the past)
+			#date = date.date().isoformat()
 
 		if have_error :
 			self.render("addTask.html", **params)
 		else:
-			
-			if creatorLoginID != None:
-				creatorLoginID=int(creatorLoginID)
-				creatorLogin = User.get_by_id(creatorLoginID).login
-				self.response.write(str(self.date))
-
-				TaskOffer.create(self.title, self.comment, creatorLogin, self.date.date(), False, self.address)
-			self.redirect("/")
+			creatorLogin = User.get_by_id(int(creatorID)).login
+			TaskOffer.create(title, comment, creatorLogin, date.date(), False, address)
+			self.redirect('/')
 
