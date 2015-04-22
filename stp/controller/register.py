a2e107bca3b1c8f606ce4c8db2658e75803883b1
Remi Even <even.remi@gmail.com>
2015-04-22 16:37:29 +0200

    Removed useless self. in register.py

--- stp/controller/register.py
+++ stp/controller/register.py
@@ -13,39 +13,38 @@ class Register(Handler):
 
 	def post(self):
 
-		self.login = self.request.get("login")
-		self.email = self.request.get("email")
-		self.password = self.request.get("password")
-		self.confirmation = self.request.get("confirmation")
+		login = self.request.get("login")
+		email = self.request.get("email")
+		password = self.request.get("password")
+		confirmation = self.request.get("confirmation")
 
-		params = dict(login = self.login,
-					  email = self.email)
+		params = dict(login = login,
+					  email = email)
 		have_error = False
 
-		if not inputValidation.valid_username(self.login):
+		if not inputValidation.valid_username(login):
 			params["error_login"] = "Le login doit etre compose de 3 a 20 chiffres/lettres."
 			have_error = True
 
-		if not inputValidation.valid_password(self.password):
+		if not inputValidation.valid_password(password):
 			params["error_password"] = "Le mot de passe n'est pas correct."
 			have_error = True
 
-		if self.password != self.confirmation:
+		if password != confirmation:
 			params["error_confirmation"] = "Le second mot de passe n'est pas identique au premier."
 			have_error = True
 
-		if not inputValidation.valid_email(self.email):
+		if not inputValidation.valid_email(email):
 			params["error_email"] = "L'email n'est pas correct."
 			have_error = True
 
-		if not have_error and User.findByLogin(self.login) != None :
+		if not have_error and User.findByLogin(login) != None :
 			params["error_login"] = "Ce login existe deja."
 			have_error = True
 
 		if have_error :
 			self.render("signup.html", **params)
 		else:
-			userId = User.create(self.login, self.password, self.email)
+			userId = User.create(login, password, email)
 			cookie.set_secure(self, "userId", str(userId))
 			self.redirect("/")
-
