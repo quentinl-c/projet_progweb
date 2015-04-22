e0309664033005941570288aa1cba90e9dc60deb
Remi Even <even.remi@gmail.com>
2015-04-22 16:29:46 +0200

    Added info in the addInfo form

--- stp/controller/userInfo.py
+++ stp/controller/userInfo.py
@@ -1,7 +1,23 @@
 from handler import Handler
 from user import User
 
-class UserInfo(Handler):
+import cookie
 
+class UserInfo(Handler):
 	def get(self):
-		self.render("addInfo.html")
+		if not(cookie.is_valid_and_secure(self, "userId")) :
+			self.redirect('/login')
+
+		userId = cookie.read_secure(self, "userId")
+		user = User.get_by_id(int(userId))
+
+		params = dict(login = user.login,
+			email = user.email)
+
+		params["lastName"] = user.lastName
+		params["firstName"] = user.firstName
+		params["phoneNumber"] = user.phoneNumber
+		params["address"] = user.address
+	#	params["birthDate"] = str(get_or_default(user.birthDate)) #TODO format that
+
+		self.render("addInfo.html", auth = True, **params)
