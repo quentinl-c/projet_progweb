e564e26922f0c291e69e7ba24402e804b6704e0e
Remi Even <even.remi@gmail.com>
2015-04-24 23:03:34 +0200

    Updated profil form

--- stp/controller/profil.py
+++ stp/controller/profil.py
@@ -2,6 +2,7 @@ from handler import Handler
 from user import User
 from TaskOffer import TaskOffer
 from Task import Task
+from opt import opt
 
 import cookie
 
@@ -10,6 +11,7 @@ class Profil(Handler):
 	def get(self):
 		if not(cookie.is_valid_and_secure(self, "userId")) :
 			self.redirect('/login')
+			return
 
 		userId = cookie.read_secure(self, "userId")
 		user = User.get_by_id(int(userId))
@@ -18,19 +20,16 @@ class Profil(Handler):
 			email = user.email,
 			points = user.points)
 
-		params["lastName"] = get_or_default(user.lastName)
-		params["firstName"] = get_or_default(user.firstName)
-		params["phoneNumber"] = get_or_default(user.phoneNumber)
-		params["address"] = get_or_default(user.address)
-		params["birthDate"] = str(get_or_default(user.birthDate))
+		params["lastName"] = opt(user.lastName, "Non renseigne(e)")
+		params["firstName"] = opt(user.firstName, "Non renseigne(e)")
+		params["phoneNumber"] = opt(user.phoneNumber, "Non renseigne(e)")
+		params["address"] = opt(user.address, "Non renseigne(e)")
+		if (user.birthDate == None):
+			params["birthDate"] = "Non renseigne(e)"
+		else :
+			params["birthDate"] = str(user.birthDate.date())
 
 		params["taskOffer"] = TaskOffer.findByCreator(userId)
 		params["task"] = Task.findByproviderLogin(user.login) 
 
 		self.render("profil.html", auth = True, **params)
-
-def get_or_default(param) :
-	if param == None :
-		return "Non renseigne(e)"
-	else :
-		return param
