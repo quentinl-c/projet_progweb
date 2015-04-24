fc5996b697233879ae6c790f11de1655984b6f96
Remi Even <even.remi@gmail.com>
2015-04-24 22:55:44 +0200

    Added birthday support in userInfo form

--- stp/controller/userInfo.py
+++ stp/controller/userInfo.py
@@ -3,11 +3,15 @@ from user import User
 
 import cookie
 import inputValidation
+from opt import opt
+
+from datetime import datetime
 
 class UserInfo(Handler):
 	def get(self):
 		if not(cookie.is_valid_and_secure(self, "userId")) :
 			self.redirect('/login')
+			return
 
 		userId = cookie.read_secure(self, "userId")
 		user = User.get_by_id(int(userId))
@@ -19,8 +23,8 @@ class UserInfo(Handler):
 		params["firstName"] = user.firstName
 		params["phoneNumber"] = user.phoneNumber
 		params["address"] = user.address
-	#	params["birthDate"] = str(get_or_default(user.birthDate)) #TODO format that
-
+		if user.birthDate != None :
+			params["birthday"] = str(user.birthDate) #TODO format that
 		self.render("addInfo.html", auth=True, **params)
 
 	def post(self):
@@ -35,11 +39,11 @@ class UserInfo(Handler):
 		phoneNumber = self.request.get("phoneNumber")
 		address = self.request.get("address")
 		email = self.request.get("email")
-		day = self.request.get("day")
-		month = self.request.get("month")
-		year = self.request.get("year")
+		date = self.request.get("birthday")
 
 		have_error = False
+
+		params = dict(login = user.login)
 
 		if not inputValidation.valid_email(email):
 			params["error_email"] = "L'email n'est pas correct."
@@ -55,9 +59,26 @@ class UserInfo(Handler):
 			user.phoneNumber = phoneNumber
 		if address:
 			user.address = address
-		#TODO : verify day, month and year, and if appropriate change the birthday of the user accordingly
+		if date :
+			try :
+				date = datetime.strptime(date, "%Y-%m-%d")
+				if datetime.today().date() < date.date() :
+					params["error_date"] = "Vous ne venez pas du futur. [insert any reference to Back to the Future here]"
+					have_error = True
+				else :
+					user.birthDate = date
+ 			except ValueError as e:
+ 				params["error_date"] = "La date de naissance n'est pas dans un format correct."
+ 				have_error = True
 		if not(have_error):
 			user.put()
 			self.redirect('/profil')
 		else:
+			params["global_error"] = "Erreur : vos modifications n'ont pu etre enregistrees."
+			params["lastName"] = opt(lastName, user.lastName)
+			params["email"] = opt(email, user.email)
+			params["firstName"] = opt(firstName, user.firstName)
+			params["phoneNumber"] = opt(phoneNumber, user.phoneNumber)
+			params["address"] = opt(address, user.address)
+			params["birthday"] = str(opt(date, (opt(user.birthDate, ""))))
 			self.render("addInfo.html", auth=True, **params)
