1208e9e4e421fdd2a7fb349c20e84f9efb17ae53
Quentin Laporte-Chabasse <quentin.laporte-chabasse@telecomnancy.net>
2015-04-26 10:52:59 +0200

    Fix a bug for the front page

--- stp/controller/front.py
+++ stp/controller/front.py
@@ -7,11 +7,11 @@ import cookie
 class Front(Handler):
 
 	def get(self):
+		tasks = TaskOffer.all()
 		if cookie.is_valid_and_secure(self, "userId") :
 			userId = cookie.read_secure(self, "userId")
 			user = User.get_by_id(int(userId))
 			login = user.login
-			tasks = TaskOffer.all()
 			self.render("front.html", auth = True, login = login, tasks = tasks[0:10])
 		else:
-			self.render("front.html", auth = False, login = None)
+			self.render("front.html", auth = False, login = None, tasks = tasks[0:10])
