ab9e489360d4f44fb720e182bd4024bab32b316f
DamienKemberg <damien.kemberg@gmail.com>
2015-05-08 20:41:56 +0200

    Fix the amount of points added to the right provider

--- stp/controller/acceptProvider.py
+++ stp/controller/acceptProvider.py
@@ -9,13 +9,12 @@ from google.appengine.ext import db
 
 class AcceptProvider(Handler):
 	def get(self,id):
-		tasks = Task.findByTaskOfferId(int(id))
+		task = Task.get_by_id(int(id))
 
-		for task in tasks:
-			userss = User.findByLogin(task.providerLogin)
-			userss.points = userss.points + 5
-			userss.put()
-			task.accepted = True
-			db.put(task)
-			redir = "/taskDashboard/"+ str(task.taskOfferId)
-			self.redirect(redir)
+		userss = User.findByLogin(task.providerLogin)
+		userss.points = userss.points + 5
+		userss.put()
+		task.accepted = True
+		db.put(task)
+		redir = "/taskDashboard/"+ str(task.taskOfferId)
+		self.redirect(redir)
